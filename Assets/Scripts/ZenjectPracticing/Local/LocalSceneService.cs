66828ef0370ed22a628e36dafe7931bf45c979d1
Pimping <auagfuiawg@mail.ru>
2026-05-05 07:45:06 +0300

    Checked additive scene loading use case

--- Assets/Scripts/ZenjectPracticing/Local/LocalSceneService.cs
+++ Assets/Scripts/ZenjectPracticing/Local/LocalSceneService.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace ZenjectPracticing.Local
 {
     public class LocalSceneService : MonoBehaviour
     {
-        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                SceneManager.LoadScene("AdditiveScene", LoadSceneMode.Additive);
+            }
+        }
     }
 }
